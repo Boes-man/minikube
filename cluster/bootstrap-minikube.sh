9a7dcbc382f89c4f99cfe73ec5a48641c957e90e
Boes-man <stompnoe@gmail.com>
2022-05-13 14:58:28 +1000

    mllb

--- cluster/bootstrap-minikube.sh
+++ cluster/bootstrap-minikube.sh
@@ -1,6 +1,6 @@
 minikube start --driver=hyperkit --container-runtime=containerd --cni=calico --bootstrapper=kubeadm --kubernetes-version=1.21.4 --addons=metallb --cpus=8 --memory=12gb --profile cluster1
 
-kubectl --context cluster1 apply -f metallb-c1.yaml
+kubectl --context cluster1 apply -f cluster/metallb-c1.yaml
 
 kubectl config use-context cluster1
 
--- clusters/bootstrap-minikube-k1.23-ephcnt.sh
+++ clusters/bootstrap-minikube-k1.23-ephcnt.sh
@@ -2,9 +2,9 @@ minikube start --driver=hyperkit --feature-gates=EphemeralContainers=true --cont
 minikube start --driver=hyperkit --feature-gates=EphemeralContainers=true --container-runtime=containerd --cni=calico --bootstrapper=kubeadm --kubernetes-version=1.23.3 --addons=metallb --cpus=6 --memory=12gb --profile cluster1
 minikube start --driver=hyperkit --feature-gates=EphemeralContainers=true --container-runtime=containerd --cni=calico --bootstrapper=kubeadm --kubernetes-version=1.23.3 --addons=metallb --cpus=6 --memory=12gb --profile cluster2
 
-kubectl --context mgmt apply -f metallb-mgmt.yaml
-kubectl --context cluster1 apply -f metallb-c1.yaml
-kubectl --context cluster2 apply -f metallb-c2.yaml
+kubectl --context mgmt apply -f clusters/metallb-mgmt.yaml
+kubectl --context cluster1 apply -f clusters/metallb-c1.yaml
+kubectl --context cluster2 apply -f clusters/metallb-c2.yaml
 
 kubectl --context mgmt label nodes mgmt topology.kubernetes.io/region=local-east-1
 kubectl --context cluster1 label nodes cluster1 topology.kubernetes.io/region=local-west-1
